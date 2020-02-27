317d2fa9e63319bf9a567d9f64d2bb8bab5b7c69
Mo Russ <mauriceruss302@gmail.com>
2020-02-26 20:31:45 -0500

    done up to get exam Scores

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,56 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private Double[] examScores;
+
+
+    public Student(String firstName, String lastName, Double[] examScores){
+        this.firstName = firstName;
+        this.lastName= lastName;
+        this.examScores= examScores;
+    }
+
+    public Student(){
+        this.firstName = null;
+        this.lastName = null;
+        this.examScores = null;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+
+
+    public void setFirstName (String firstName){
+        this.firstName = firstName;
+
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+
+        return this.examScores.length;
+    }
+
+    public String getExamScores(){
+        String scores = "Exam Scores: \n";
+        for(int i = 0; i < examScores.length; i++){
+            scores  += String.format("Exam %d -> %d \n",i +1 , (int)Math.round(this.examScores[i]) );
+
+        }
+
+        return scores;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,48 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void getExamScores() {
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        String output = student.getExamScores();
+
+        String expected = "Exam Scores: \nExam 1 -> 100 \nExam 2 -> 95 \nExam 3 -> 123 \nExam 4 -> 96 \n";
+        Assert.assertEquals(expected,output);
+    }
+
+    @Test
+    public void setFirstName() {
+       Student student = new Student();
+       String expected = "mary";
+       student.setFirstName(expected);
+       String actual = student.getFirstName();
+       Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setLastName() {
+        Student student = new Student();
+        String expected = "smith";
+        student.setLastName(expected);
+        String actual = student.getLastName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumberOfExamsTaken() {
+        Student student= new Student("jay", "spins", new Double[]{4.0, 12.6, 27.5, 8.9});
+        Assert.assertTrue(4 == student.getNumberOfExamsTaken());
+    }
+
+    @Test
+    public void testGetExamScores() {
+    }
 }
