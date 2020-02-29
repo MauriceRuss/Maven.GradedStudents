389f721833bab665b185cd9f94f3050abb735a33
Mo Russ <mauriceruss302@gmail.com>
2020-02-29 10:23:42 -0500

    changed logic for classroom.

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,36 +1,54 @@
 package io.zipcoder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Classroom {
     private Integer maxNumberOfStudents;
-    private Student[] students;
+    private ArrayList<Student> students = new ArrayList<>();
+
+
 
     public Classroom(Integer maxNumberOfStudents){
-        this.maxNumberOfStudents = maxNumberOfStudents;
+
+        this.students = new ArrayList<>(maxNumberOfStudents);
     }
 
     public Classroom(Student[] students){
-        this.students = students;
+        this.students.addAll(Arrays.asList(students));
 
     }
 
     public Classroom(){
-        this.students = new Student [30];
+
+        this.students = new ArrayList<>(30);
     }
     
     public Student[] getStudent(){
-        return students;
+        ArrayList<Student> studentList = new ArrayList<>();
+        for (Student student: students) {
+            if (student != null)
+                studentList.add(student);
+        }
+        return studentList.toArray(new Student[studentList.size()]);
+
     }
 
     public double getAverageExamScore(){
         Double avgExamScore = 0.0;
         double count = 0;
-        for(int i = 0; i < students.length; i++){
-            count += students[i].getAverageExamScore();
-            avgExamScore = count/students.length;
+        for(int i = 0; i < students.size(); i++){
+            Student pupil = students.get(i);
+            count += pupil.getNumberOfExamsTaken();
+            avgExamScore = (pupil.getAverageExamScore() * pupil.getNumberOfExamsTaken());
         }
-
         return avgExamScore;
     }
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+
+
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -3,10 +3,25 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ClassroomTest {
-    @Test
-    public void getStudent() {
-    }
+//    @Test
+//    public void getStudent() {
+//        Double[] s1Scores = { 100.0, 150.0 };
+//        Double[] s2Scores = { 225.0, 25.0 };
+//
+//        Student s1 = new Student("student", "one", s1Scores);
+//        Student s2 = new Student("student", "two", s2Scores);
+//
+//        ArrayList<Student> students = new {s1,s2};
+//        Classroom classroom = new Classroom(students);
+//
+//        ArrayList<Student> output  = new ArrayList<>(Arrays.asList(students));
+//        ArrayList<Student> actual = classroom.getStudent();
+//        Assert.assertEquals(output, actual);
+//    }
 
     @Test
     public void getAverageExamScore() {
@@ -22,4 +37,7 @@ public class ClassroomTest {
         double actual = 125.0;
         Assert.assertEquals(output, actual, 0.001);
     }
+
+
+
 }
